89ba66d812ec35e92164f33ecfcd90644cbb07ef
Caius Iliesi <caiusili2003@gmail.com>
2023-04-10 22:40:53 -0400

    Project_CAIUS_ILIESI.java
    Added code to display all policy information the number of smokers, and the number of non-smokers.

--- Project_CAIUS_ILIESI.java
+++ Project_CAIUS_ILIESI.java
@@ -1,65 +1,112 @@
 // File Name: Project_CAIUS_ILIESI
 
 // Developer Name: CAIUS ILIESI
-// Date: 3/27/2023
+// Date: 4/10/2023
 
-// This is the Demo Class, but I did not know whether to change the file name or not, so I left it to Project_firstname_lastname
+/* This is the Demo Class. Everything went well until the last step when I tested the program. The "Compile File" button did not give me any errors,
+   but when running the program, I was met with the error "Exception in thread "main" java.lang.NumberFormatException: For input string: ""."
+   I rewrote the demo and policy classes several times, but I was presented with the same error every time. I also tried looking through my textbook 
+   and online for how to address this issue, but I couldn't find anything to help me. 
+   
+   I'm confident the program would originally work and that it can read the .txt file, but I couldn't find a way to fix or bypass this issue. 
+   If you know what the problem is, I would really appreciate to see how I could fix it, because I feel very embarrased for submitting a program that 
+   can't run.
 
+*/
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
-public class Project_CAIUS_ILIESI
-{
-public static void main(String[] args)
-{
 
-// Declare variables
-int pNumber;
-String pName;
-String fName;
-String lName;
-int age;
-boolean isSmoker;
-double height;
-double weight;
+public class Project_CAIUS_ILIESI {
 
-// Create an instance of Scanner class
-Scanner console=new Scanner(System.in);
-System.out.print("Please enter the Policy Number: ");
-pNumber=Integer.parseInt(console.nextLine());
-System.out.print("Please enter the Provider Name: ");
-pName=console.nextLine();
-System.out.print("Please enter the Policyholder's First Name: ");
-fName=console.nextLine();
-System.out.print("Please enter the Policyholder's Last Name: ");
-lName=console.nextLine();
-System.out.print("Please enter the Policyholder's Age: ");
-age=Integer.parseInt(console.nextLine());
-System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-String smoker=console.nextLine();
-if(smoker.equals("smoker"))
-isSmoker=true;
-else
-isSmoker=false;
+    /**
+     * Reads the policy information from the specified text file and returns an ArrayList containing Policy objects.
+     * 
+     * @param fileName the name of the text file containing the policy information
+     * @return an ArrayList containing Policy objects
+     * @throws FileNotFoundException if the specified file does not exist
+     */
+     
+    public static ArrayList<Policy> readPolicyInformation(String fileName) throws FileNotFoundException {
+        ArrayList<Policy> policies = new ArrayList<>();
+        Scanner input = new Scanner(new File(fileName));
+        while (input.hasNextLine()) {
+            int policyNumber = Integer.parseInt(input.nextLine());
+            String providerName = input.nextLine();
+            String firstName = input.nextLine();
+            String lastName = input.nextLine();
+            int age = Integer.parseInt(input.nextLine());
+            boolean isSmoker = input.nextLine().equals("smoker");
+            double height = Double.parseDouble(input.nextLine());
+            double weight = Double.parseDouble(input.nextLine());
+            Policy policy = new Policy(policyNumber, providerName, firstName, lastName, age, isSmoker, height, weight);
+            policies.add(policy);
+        }
+        input.close();
+        return policies;
+    }
 
-System.out.print("Please enter the Policyholder's Height (in inches): ");
-height=Double.parseDouble(console.nextLine());
-System.out.print("Please enter the Policyholder's Weight (in pounds):");
-weight=Double.parseDouble(console.nextLine());
+    /**
+     * Displays the policy information for the specified Policy object.
+     * 
+     * @param policy the Policy object to display information for
+     */
+     
+    public static void displayPolicyInformation(Policy policy) {
+        System.out.println("Policy Number: " + policy.getpNumber());
+        System.out.println("Provider Name: " + policy.getpName());
+        System.out.println("Policyholder's First Name: " + policy.getfName());
+        System.out.println("Policyholder's Last Name: " + policy.getlName());
+        System.out.println("Policyholder's Age: " + policy.getAge());
+        System.out.println("Policyholder's Smoking Status(smoker/non-smoker): " + (policy.isSmoker() ? "smoker" : "non-smoker"));
+        System.out.println("Policyholder's Height : " + policy.getHeight()+" inches");
+        System.out.println("Policyholder's Weight : " + policy.getWeight()+" pounds");
+        System.out.println("Policyholder's BMI: " + String.format("%.2f",policy.getBMI()));
+        System.out.println("Policy Price: $" + String.format("%.2f",policy.getPolicyPrice()));
+        System.out.println();
+    }
 
-// Create a Policy object
-Policy policy=new Policy(pNumber, pName, fName, lName, age, isSmoker, height, weight);
+    /**
+     * Displays the number of policyholders that are smokers and the number of
+     * policyholders that are non-smokers for the specified list of Policy objects.
+     * 
+     * @param policies the list of Policy objects to analyze
+     */
+     
+    public static void displaySmokerInformation(ArrayList<Policy> policies) {
+        int smokers = 0;
+        int nonSmokers = 0;
+        for (Policy policy : policies) {
+            if (policy.isSmoker()) {
+                smokers++;
+            } else {
+                nonSmokers++;
+            }
+        }
+        System.out.println("The number of policies with a smoker is: " + smokers);
+        System.out.println("The number of policies with a non-smoker is: " + nonSmokers);
+    }
 
-System.out.println();
-System.out.printf("Policy Number: %d\n",policy.getpNumber());
-System.out.printf("Provider Name: %s\n",policy.getpName());
-System.out.printf("Policyholder's First Name: %s\n",policy.getfName());
-System.out.printf("Policyholder's Last Name: %s\n",policy.getlName());
-System.out.printf("Policyholder's Age: %d\n",policy.getAge());
-System.out.printf("Policyholder's Smoking Status: %s\n",policy.isSmoker());
-System.out.printf("Policyholder's Height: %.2f\n",policy.getHeight());
-System.out.printf("Policyholder's Weight: %.2f\n",policy.getWeight());
-System.out.printf("Policyholder's BMI: %.2f\n",policy.getBMI());
-System.out.printf("Policy Price: $%.2f\n",policy.getpolicyprice());
+    /**
+     * The main method of the Demo class.
+     * 
+     * @param args command-line arguments (not used)
+     */
+     
+    public static void main(String[] args) {
+        try {
+            ArrayList<Policy> policies = readPolicyInformation("PolicyInformation.txt");
+            for (Policy policy : policies) {
+                displayPolicyInformation(policy);
+            }
+            displaySmokerInformation(policies);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
+    }
 
 }
-}
+
+
