df7ef8198706b9b6d700d5e532d80f905585ed04
Caius Iliesi <caiusili2003@gmail.com>
2023-04-10 22:22:01 -0400

    Update Policy.java
    Added documentation comments.

--- Policy.java
+++ Policy.java
@@ -1,145 +1,146 @@
 // File Name: Policy.java
 
 // Developer Name: CAIUS ILIESI
-// Date: 3/27/2023
+// Date: 4/10/23
 
 
-public class Policy
-{
+public class Policy {
 
-// Private instance variabels
-private int pNumber;
-private String pName;
-private String fName;
-private String lName;
-private int age;
-private boolean isSmoker;
-private double height;
-private double weight;
+    private int pNumber;
+    private String pName;
+    private String fName;
+    private String lName;
+    private int age;
+    private boolean isSmoker;
+    private double height;
+    private double weight;
 
-// Constructor that arguments
-public Policy(int pNumber, String pName, String fName, String lName, int age, boolean isSmoker, double height,
-double weight) {
-this.pNumber = pNumber;
-this.pName = pName;
-this.fName = fName;
-this.lName = lName;
-this.age = age;
-this.isSmoker = isSmoker;
-this.height = height;
-this.weight = weight;
+    /**
+     * Constructor with arguments.
+     *
+     * @param pNumber   The policy number.
+     * @param pName     The provider name.
+     * @param fName     The policyholder's first name.
+     * @param lName     The policyholder's last name.
+     * @param age       The policyholder's age.
+     * @param isSmoker  The policyholder's smoking status (true if smoker, false if non-smoker).
+     * @param height    The policyholder's height in inches.
+     * @param weight    The policyholder's weight in pounds.
+     */
+     
+    public Policy(int pNumber, String pName, String fName, String lName, int age, boolean isSmoker, double height,
+                  double weight) {
+        this.pNumber = pNumber;
+        this.pName = pName;
+        this.fName = fName;
+        this.lName = lName;
+        this.age = age;
+        this.isSmoker = isSmoker;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    /**
+     * Calculates the policyholder's body mass index (BMI).
+     *
+     * @return The policyholder's BMI.
+     */
+    public double getBMI() {
+        return (weight * 703) / (height * height);
+    }
+
+    /**
+     * Calculates the price of the policy.
+     *
+     * @return The price of the policy.
+     */
+    public double getPolicyPrice() {
+        double policyCost = 0;
+
+        int BASE_PRICE = 600;
+        int ADD_FEE = 75;
+        int SMOKER_FEE = 100;
+
+        policyCost = policyCost + BASE_PRICE;
+
+        if (age > 50) {
+            policyCost = policyCost + ADD_FEE;
+        }
+
+        if (isSmoker) {
+            policyCost = policyCost + SMOKER_FEE;
+        }
+
+        if (getBMI() > 35) {
+            policyCost = policyCost + (getBMI() - 35) * 20;
+        }
+
+        return policyCost;
+    }
+
+    public int getpNumber() {
+        return pNumber;
+    }
+
+    public void setpNumber(int pNumber) {
+        this.pNumber = pNumber;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isSmoker() {
+        return isSmoker;
+    }
+
+    public void setSmoker(boolean isSmoker) {
+        this.isSmoker = isSmoker;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 }
 
-// Calculate BMI
-public double getBMI()
-{
-return (weight * 703 ) / (height*height );
-}
 
-// Calculate policy price
-public double getpolicyprice()
-{
-double policy_cost=0;
-
-int BASE_PRICE=600;
-int ADD_FEE=75;
-int SMOKER_FEE=100;
-
-policy_cost=policy_cost+BASE_PRICE;
-
-// Check if age is above 50
-if(age>50)
-policy_cost=policy_cost+ADD_FEE;
-
-// Check person is smoker
-if(isSmoker)
-policy_cost=policy_cost+SMOKER_FEE;
-
-// Check BMI is over 35
-if(getBMI()>35)
-policy_cost=policy_cost+( getBMI() - 35 ) * 20 ;
-
-// Return policy cost
-return policy_cost;
-}
-
-public int getpNumber()
-{
-return pNumber;
-}
-
-public void setpNumber(int pNumber)
-{
-this.pNumber = pNumber;
-}
-
-public String getpName()
-{
-return pName;
-}
-
-public void setpName(String pName)
-{
-this.pName = pName;
-}
-
-public String getfName()
-{
-return fName;
-}
-
-public void setfName(String fName)
-{
-this.fName = fName;
-}
-
-public String getlName()
-{
-return lName;
-}
-
-public void setlName(String lName)
-{
-this.lName = lName;
-}
-
-public int getAge()
-{
-return age;
-}
-
-public void setAge(int age)
-{
-this.age = age;
-}
-
-public boolean isSmoker()
-{
-return isSmoker;
-}
-
-public void setSmoker(boolean isSmoker)
-{
-this.isSmoker = isSmoker;
-}
-
-public double getHeight()
-{
-return height;
-}
-
-public void setHeight(double height)
-{
-this.height = height;
-}
-
-public double getWeight()
-{
-return weight;
-}
-
-public void setWeight(double weight)
-{
-this.weight = weight;
-}
-}
