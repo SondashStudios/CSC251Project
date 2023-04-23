5b25e6f5af2585fe1f60932682555a5e52d40965
Caius Iliesi <caiusili2003@gmail.com>
2023-04-23 18:00:05 -0400

    Update Policy.java
    Added toString method, Added static field to track number of Policy objects

--- Policy.java
+++ Policy.java
@@ -1,59 +1,27 @@
 // File Name: Policy.java
 
 // Developer Name: CAIUS ILIESI
-// Date: 4/10/23
+// Date: 4/23/23
 
 
 public class Policy {
 
     private int pNumber;
     private String pName;
-    private String fName;
-    private String lName;
-    private int age;
-    private boolean isSmoker;
-    private double height;
-    private double weight;
+    private PolicyHolder policyHolder;
+    private static int policyCount = 0;
 
-    /**
-     * Constructor with arguments.
-     *
-     * @param pNumber   The policy number.
-     * @param pName     The provider name.
-     * @param fName     The policyholder's first name.
-     * @param lName     The policyholder's last name.
-     * @param age       The policyholder's age.
-     * @param isSmoker  The policyholder's smoking status (true if smoker, false if non-smoker).
-     * @param height    The policyholder's height in inches.
-     * @param weight    The policyholder's weight in pounds.
-     */
-     
-    public Policy(int pNumber, String pName, String fName, String lName, int age, boolean isSmoker, double height,
-                  double weight) {
+    public Policy(int pNumber, String pName, PolicyHolder policyHolder) {
         this.pNumber = pNumber;
         this.pName = pName;
-        this.fName = fName;
-        this.lName = lName;
-        this.age = age;
-        this.isSmoker = isSmoker;
-        this.height = height;
-        this.weight = weight;
+        this.policyHolder = policyHolder;
+        policyCount++;
     }
 
-    /**
-     * Calculates the policyholder's body mass index (BMI).
-     *
-     * @return The policyholder's BMI.
-     */
     public double getBMI() {
-        return (weight * 703) / (height * height);
+        return (policyHolder.getWeight() * 703) / (policyHolder.getHeight() * policyHolder.getHeight());
     }
 
-    /**
-     * Calculates the price of the policy.
-     *
-     * @return The price of the policy.
-     */
     public double getPolicyPrice() {
         double policyCost = 0;
 
@@ -62,15 +30,12 @@ public class Policy {
         int SMOKER_FEE = 100;
 
         policyCost = policyCost + BASE_PRICE;
-
-        if (age > 50) {
+        if (policyHolder.getAge() > 50) {
             policyCost = policyCost + ADD_FEE;
         }
-
-        if (isSmoker) {
+        if (policyHolder.isSmoker()) {
             policyCost = policyCost + SMOKER_FEE;
         }
-
         if (getBMI() > 35) {
             policyCost = policyCost + (getBMI() - 35) * 20;
         }
@@ -78,10 +43,13 @@ public class Policy {
         return policyCost;
     }
 
+    public static int getPolicyCount() {
+        return policyCount;
+    }
+
     public int getpNumber() {
         return pNumber;
     }
-
     public void setpNumber(int pNumber) {
         this.pNumber = pNumber;
     }
@@ -94,53 +62,20 @@ public class Policy {
         this.pName = pName;
     }
 
-    public String getfName() {
-        return fName;
+    public PolicyHolder getPolicyHolder() {
+        return policyHolder;
     }
 
-    public void setfName(String fName) {
-        this.fName = fName;
+    public void setPolicyHolder(PolicyHolder policyHolder) {
+        this.policyHolder = policyHolder;
     }
 
-    public String getlName() {
-        return lName;
-    }
-
-    public void setlName(String lName) {
-        this.lName = lName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public boolean isSmoker() {
-        return isSmoker;
-    }
-
-    public void setSmoker(boolean isSmoker) {
-        this.isSmoker = isSmoker;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
+    @Override
+    public String toString() {
+        return "Policy Number: " + pNumber +
+                "\nProvider Name: " + pName +
+                "\n" + policyHolder.toString() +
+                "\nPolicyholder's BMI: " + String.format("%.2f", getBMI()) +
+                "\nPolicy Price: $" + String.format("%.2f", getPolicyPrice());
     }
 }
-
-
